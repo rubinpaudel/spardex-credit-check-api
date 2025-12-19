d833859fbdc646e6a43c017871fcf669756c4349
Rubin Paudel <paudelrubin18@gmail.com>
2025-12-19 18:17:08 +0100

    cleanup

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,19 +4,4 @@ import { creditCheckRoutes } from "./credit-check";
 
 export const routes = new Elysia()
   .use(healthRoutes)
-  .use(creditCheckRoutes)
-  .get(
-    "/",
-    () => ({
-      message: "Welcome to Spardex API",
-      version: "1.0.0",
-      documentation: "/swagger",
-    }),
-    {
-      detail: {
-        tags: ["API"],
-        summary: "API root",
-        description: "Returns basic API information",
-      },
-    }
-  );
+  .use(creditCheckRoutes);
